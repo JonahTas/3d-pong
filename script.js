ca9bf8a65cc937cff51e0bb26e6d89143ccdd4eb
JonahTas <90318569+JonahTas@users.noreply.github.com>
2021-09-27 13:14:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,88 @@
+var x, y, v, b, s, sc, s2, dot1, score;
+var gameState = 0
+
+function setup() {
+  createCanvas(500, 500);
+  x = 200;
+  y = 200;
+  v = 2.4
+  b = 1
+  s = 40
+  sc = 0.75
+}
+
+function draw() {
+
+  if (gameState == 0) {
+
+    textSize(40);
+    fill(0);
+    text('Press shift to start', 70, 300);
+
+  }
+  if (gameState == 1) {
+
+    background(240)
+    distance = dist(mouseX, mouseY, x, y)
+    if (distance < s2) {
+      dot1 = 0
+      score = 1
+    }
+    else {
+      dot1 = 255
+      score = 0
+    }
+    //Zorgt dat het midden van kleur kan veranderen als je er over hovert
+
+
+    fill(250, 84, 75)
+    circle(x, y, s);
+    //Grote stip
+
+    fill(dot1)
+    circle(x, y, s2);
+    //Kleine stip
+
+    fill(250)
+    ellipse(mouseX, mouseY, 20, 20);
+    //Cursor
+
+    x = x + v;
+    y = y + b
+    s = s + sc
+    s2 = s * 0.4
+
+    if (x < 100 || x > 400) {
+      v = v * -1;
+
+    }
+    //xborder
+
+    if (y < 100 || y > 400) {
+      b = b * -1;
+    }
+    //yborder
+
+    if (s < 20 || s > 80) {
+      sc = sc * -1;
+    }
+
+  }
+  if (gameState == 2) {
+  }
+  //print(gameState)
+  //print(score)
+}
+
+
+
+function keyPressed() {
+  if (keyCode === SHIFT) {
+    if (gameState == 0) {
+      gameState = 1;
+    }
+    if (gameState == 2){
+      gameState = 1;
+    }
+  }
+}
